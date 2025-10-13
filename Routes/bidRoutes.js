a2a47fe78540ecdbb03431123dd820a53593314b
Muhammad Usman Sarwar <105904144+theusmansarwar@users.noreply.github.com>
2025-10-13 11:34:53 +0500

    resolve blog live api issue

--- Routes/bidRoutes.js
+++ Routes/bidRoutes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { placeBid, producttop5Bid, getAllBids, softDeleteBid } = require("../Controller/bidController");
+const { placeBid, producttop5Bid, getAllBids, softDeleteMultipleBids } = require("../Controller/bidController");
 
 router.post("/", placeBid);
 router.get("/bidlist/:productId", producttop5Bid);
 router.get("/listbidders", getAllBids);
-router.post("/delete-multiple", softDeleteBid);
+router.delete("/delete-multiple", softDeleteMultipleBids);
 
 module.exports = router;
